f34278e34633143e2fb279189b04289526bbfae6
Dhaval Thacker <dreamsatgrims@gmail.com>
2026-06-29 00:57:10 +0530

    Added app dependencies

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("🕉️ Utsav"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to Utsav\nModern Hindu Calendar",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
